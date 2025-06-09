c1651239af7a1fd1c929e579e258918670378c12
danielosman84 <daniel.osman@live.co.uk>
2025-06-09 17:03:31 +0100

    fix: use relative imports for Button and Card

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,6 +1,5 @@
 import { Button } from "../components/ui/button";
 import { Card, CardContent } from "../components/ui/card";
-
 import Image from "next/image";
 
 export default function HomePage() {
