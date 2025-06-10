05e255108148f32e4c941c78db7354d9d4447c5f
danielosman84 <daniel.osman@live.co.uk>
2025-06-10 10:13:52 +0100

    Fix import casing and update HomePage

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,5 +1,5 @@
-import { Button } from "../components/ui/Button";
-import { Card, CardContent } from "../components/ui/Card";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
 import Image from "next/image";
 
 export default function HomePage() {
